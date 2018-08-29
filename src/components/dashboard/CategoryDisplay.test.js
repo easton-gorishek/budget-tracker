49096027bb16db92e5472f02c643ede6a00a7bc4
Easton Gorishek <ieaston26@gmail.com>
2018-08-29 12:25:00 -0700

    fixed snapshot test

--- src/components/dashboard/CategoryDisplay.test.js
+++ src/components/dashboard/CategoryDisplay.test.js
@@ -1,4 +1,4 @@
-import { CategoryDisplay } from './CategoryDisplay';
+import  CategoryDisplay  from './CategoryDisplay';
 import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
@@ -7,12 +7,10 @@ describe('category display', () => {
 
   it('renders display, then edit', () => {
     const handleEdit = jest.fn();
-    const handleRemove = jest.fn();
 
     const wrapper = shallow(<CategoryDisplay
       category={{ name: 'name', budget: 'budget' }}
       onEdit={handleEdit}
-      remove={handleRemove}
     />);
 
     expect(toJSON(wrapper)).toMatchSnapshot();
@@ -21,11 +19,5 @@ describe('category display', () => {
     const editCalls = handleEdit.mock.calls;
     expect(editCalls.length).toBe(1);
 
-    wrapper.find('button[name="remove"]').simulate('click');
-    const removeCalls = handleEdit.mock.calls;
-    expect(removeCalls.length).toBe(1);
-
   });
-
-
 });
